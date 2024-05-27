f2654ab4299a1ec61d95bc66df1714dd540a9372
Novolin <captainnovolin@gmail.com>
2024-05-27 13:09:21 -0400

    more fsk work

--- Pico/pico_fsk.py
+++ Pico/pico_fsk.py
@@ -10,9 +10,11 @@ buffered_input = array.array("H")
 plan:
 sample some input (10ms? at 8khz? that's 80 samples?)
 
-
-
-
-
-
 '''
+
+
+class TTYReader:
+    def __init__(self, pin, baudrate):
+        self.baudrate = baudrate
+        self.pin = pin
+        self.in_buff = array.array("H")
--- wavreadtext.py
+++ wavreadtext.py
@@ -75,41 +75,46 @@ FIGS = (
 
 class AudioReader:
     def __init__(self, baudrate):
-        if baudrate == 50:
-            self.bit_size = 882
-        else:
-            self.bit_size = 970
+        self.bit_size = int((1/baudrate)*44100)
+        print(self.bit_size)
         self.src = False
         self.pitch = aubio.pitch(hop_size = self.bit_size)
-        self.byte_length = self.bit_size * 7 # 5 bit byte + start and stop bits.
+        self.byte_length = int(self.bit_size * 7.5) # 5 bit byte + start and stop bits.
         self.rawdata = []
         
     def set_input_source(self, filename):
         self.src = aubio.source(filename, hop_size = self.bit_size)
+        self.src.seek(0)
 
     def get_data_packets(self):
         # take the audio and pull out the data packets:
         awaiting_start_bit = True
+        silent = True
         bybuff = 0b11111
         bcount = 7
         while True: # iterate the whole file
-            data, count = self.src.do()
             if awaiting_start_bit:
+                for frame in self.src:
+                    if frame != 0:
+                        data, count = self.src.do()
+                        break
+
                 if self.pitch(data) > 1700:
                     awaiting_start_bit = False
                     bybuff = 0
                     bcount = 0
             else:
-                if bcount < 6: 
+                data, count = self.src.do()
+                if bcount < 5: 
                     bybuff = bybuff << 1
                     bcount += 1
                     if self.pitch(data) > 1700:
                         bybuff = bybuff | 0
                     else:
                         bybuff = bybuff | 1
-                    if bcount == 6:
-                        self.rawdata.append(bybuff)
-                        awaiting_start_bit = True
+                if bcount == 5:
+                    self.rawdata.append(bybuff)
+                    awaiting_start_bit = True
             if count < self.bit_size:
                 break
 
@@ -117,10 +122,11 @@ class AudioReader:
         if not self.src: # if file isn't set
             print("Error: Select a file to read")
             return False
-        if self.rawdata == []:
-            self.get_data_packets()
+        self.rawdata = []
+        self.get_data_packets()
         out_str = ""
         ltrs = True
+        print(self.rawdata)
         for i in self.rawdata:
             if ltrs:
                 if i == 0x1b:
@@ -142,7 +148,5 @@ class AudioReader:
                         out_str += FIGS[i]
                     except:
                         print(i)
-
-
         return out_str
 
