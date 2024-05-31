55b612916d382efedcbb0d491760b01cc9156347
Novolin <55156472+Novolin@users.noreply.github.com>
2024-05-31 00:26:04 +0000

    Pico Time!

--- Pico/pico_fsk.py
+++ Pico/pico_fsk.py
@@ -5,7 +5,7 @@ from analogio import AnalogIn
 import analogbufio
 import array
 
-buffered_input = array.array("H")
+
 analog_input = board.A0
 read_led = board.GP17
 '''
@@ -25,4 +25,29 @@ class TTYReader:
     def __init__(self, pin, baudrate):
         self.baudrate = baudrate
         self.pin = pin
-        self.in_buff = array.array("H")
+        self.in_buff = array.array("H", [0]*80) # 80 samples = 10 ms
+        self.analog_reader = False
+
+    def init_reader(self):
+        try:
+            self.analog_reader = analogbufio.BufferedIn(self.pin, sample_rate = 8000) # 8khz samples should be ok?
+            return True
+        except:
+            return False
+
+    def read_data(self):
+        if self.analog_reader == False:
+            print("Error: Initialize reader first!!")
+            return False
+        self.analog_reader.readinto(self.in_buff)
+        return self.in_buff
+
+    def deinit_reader(self):
+        if self.analog_reader:
+            self.analog_reader.deinit()
+        self.analog_reader = False
+        return
+
+    def get_sampled_freqency(self):
+        # count zero crossings.
+        pass
