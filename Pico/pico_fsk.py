a976a292bdb1a6aa087d2a94450d1e214ca7b43f
Novolin <55156472+Novolin@users.noreply.github.com>
2024-05-31 00:29:08 +0000

    more pico :)

--- Pico/pico_fsk.py
+++ Pico/pico_fsk.py
@@ -4,6 +4,7 @@ import board
 from analogio import AnalogIn
 import analogbufio
 import array
+import asyncio # oh no async stuff oof ouch
 
 
 analog_input = board.A0
@@ -28,17 +29,18 @@ class TTYReader:
         self.in_buff = array.array("H", [0]*80) # 80 samples = 10 ms
         self.analog_reader = False
 
-    def init_reader(self):
-        try:
-            self.analog_reader = analogbufio.BufferedIn(self.pin, sample_rate = 8000) # 8khz samples should be ok?
-            return True
-        except:
-            return False
+    def init_monitor(self):
+        pass 
+    def check_for_silence(self):
+        # init analog reader
+        # if line is silent, return false
+        # else return true
+        pass
 
     def read_data(self):
-        if self.analog_reader == False:
-            print("Error: Initialize reader first!!")
-            return False
+        if self.analog_reader != False:
+            self.analog_reader.deinit() # Nuke any previous analog input
+        self.analog_reader = analogbufio.BufferedIn(self.pin, sample_rate = 8000) # 8khz samples should be ok?
         self.analog_reader.readinto(self.in_buff)
         return self.in_buff
 
