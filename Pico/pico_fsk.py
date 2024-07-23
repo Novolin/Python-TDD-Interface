9b54361a648f1c51c3779e898e3946f828cc1134
Novolin <captainnovolin@gmail.com>
2024-07-23 15:23:59 -0400

    Fixed instant timeout, maybe some reading errors?

--- Pico/pico_fsk.py
+++ Pico/pico_fsk.py
@@ -7,82 +7,13 @@ import array
 from supervisor import ticks_ms
 from time import sleep
 
-
-
 class TTYReader:
-    def __init__(self, pin, baudrate):
-        self.baudrate = baudrate
-        self.pin = pin
-        self.in_buff = array.array("H", [0]*80) # 80 samples = 10 ms
-        self.analog_reader = False
-        self.incoming_data = "" # String recieved from the TTY
-        self.zero_point = 0
-
-    def init_monitor(self):
-        self.deinit_reader() # make sure there isn't already something running
-        self.analog_reader = AnalogIn(self.pin)
-        
-    def deinit_reader(self):
-        try:
-            self.analog_reader.deinit()
-        except:
-            print("No reader to deinitialize. Continuing.")
-        self.analog_reader = False
-        return
-
-    def check_for_silence(self, threshold = 200):
-        if self.analog_reader.value > threshold:
-            # WE GOT A BYTE!
-            return True
-        return False # no noise :)
-
-    def read_data(self):
-        self.analog_reader.readinto(self.in_buff)
-        return self.in_buff
-
-    def init_reader(self):
-        self.deinit_reader()
-        self.analog_reader = analogbufio.BufferedIn(self.pin, sample_rate = 8000) # 8kHz sample rate, we don't need a super high res?
-
-
-    def get_sampled_freqency(self):
-        # Count zero crossings in our sample
-        zero_crossings = 0
-        current_sign = 1
-        for sample in self.in_buff:
-            if sample * current_sign < 0:
-                zero_crossings += 1
-                current_sign = current_sign * -1
-        # do some basic math:
-        if zero_crossings > 32:
-            return 0 # 1800
-        else:
-            return 1 # 1400
-        
-    def read_full_byte(self):
-        # Read an entire byte of data:
-        byte_buffer = 0
-        self.init_reader()
-        start_time = ticks_ms()
-        for b in range(6):    
-            self.read_data()
-            byte_buffer &= self.get_sampled_frequency()
-            byte_buffer = byte_buffer << 1
-            while ticks_ms() < start_time + (b * 20):
-                pass # wait for next bit to start
-        return byte_buffer
-
-    def calibrate(self):
-        pass
-
-
-class TTYReader2:
     # redo that should work better now
 
     def __init__(self, pin, baudrate):
         self.pin = pin
         self.analog_input = False # Wait for something to initialize it
-        self.input_buffer = array.array("H",[0] * 100) #10ms of samples
+        self.input_buffer = array.array("H",[0] * 100) # 5 ms of samples
         self.baudrate = baudrate
         self.bit_time = 20
         if baudrate == 45:
@@ -91,30 +22,106 @@ class TTYReader2:
         self.noise_floor = 10
         self.await_bit = True #are we waiting on the line to do something
         self.calibrate()
-        self.recieved_string = ""
+        self.received_string = ""
         self.read_byte_arr = bytearray([0] * 32) # 32 bytes should be enough?
         self.byte_pointer = 0 # which byte we're messing with in the array
         self.byte_start_time = 0 # When the last byte was read, just in case we need to do weird shit later.
+        self.LTRS = (
+            "\b",
+            "E",
+            "\n",
+            "A",
+            " ",
+            "S",
+            "I",
+            "U",
+            "\r",
+            "D",
+            "R",
+            "J",
+            "N",
+            "F",
+            "C",
+            "K",
+            "T",
+            "Z",
+            "L",
+            "W",
+            "H",
+            "Y",
+            "P",
+            "Q",
+            "O",
+            "B",
+            "G",
+            "FIGS",
+            "M",
+            "X",
+            "V",
+            "LTRS"
+            )
         
+        self.FIGS = (
+            "\b",
+            "3",
+            "\n",
+            "-",
+            " ",
+            "-",
+            "8",
+            "7",
+            "\r",
+            "$",
+            "4",
+            "'",
+            ",",
+            "!",
+            ":",
+            "(",
+            "5",
+            '"',
+            ")",
+            "2",
+            "=",
+            "6",
+            "0",
+            "1",
+            "9",
+            "?",
+            "+",
+            "FIGS",
+            ".",
+            "/",
+            ";",
+            "LTRS"
+            )
+        self.char_mode = self.LTRS # default to letter mode
 
-    def init_monitor(self):
+    def init_monitor(self): # Prepare the analog input pin for one-shot reads
         if self.analog_input != False:
             self.analog_input.deinit()
         self.analog_input = AnalogIn(self.pin)
         self.await_bit = True
         return
-    
-    def init_reader(self):
+
+    def check_for_input(self): # Returns false when input detected.
+        check = self.analog_input.value
+        if check > self.silence_level + self.noise_floor or check < self.silence_level - self.noise_floor:
+            return False 
+        return True     
+
+
+    def init_reader(self): # Prepare the analog input pin for buffered reads
         if self.analog_input != False:
             self.analog_input.deinit()
-        self.analog_input = analogbufio.BufferedIn(self.pin, sample_rate = 10000) #10kHz for easier math, and more samples = more accurate data
+        self.analog_input = analogbufio.BufferedIn(self.pin, sample_rate = 20000) 
         return
     
     def calibrate(self):
         print("Calibrating Analog Input...")
         self.init_reader()
         self.analog_input.readinto(self.input_buffer)
-        self.silence_level = sum(self.input_buffer)//100 # we know we have 100 samples. Use integers to make math faster, since timing is important
+        self.silence_level = sum(self.input_buffer)//100 # we know we have 100 samples, and don't need the precision of a proper decimal
         if self.silence_level > 3000 or self.silence_level == 0:
             print("Calibration error! Aborting!")
             return False
@@ -122,32 +129,38 @@ class TTYReader2:
             print("Calibrated! Silence threshold = " + str(self.silence_level))
             print("Beginning Monitor...")
             self.init_monitor()
-
             return True
 
     
     def read_bit_get_value(self):
-        self.analog_input.readinto(self.input_buffer)
-        # Count zero crossings in our sample
-        zero_crossings = 0
-        above_avg = True
-        if self.input_buffer[0] < self.silence_level:
-            above_avg = False
-        for i in self.input_buffer:
-            if above_avg and i < self.silence_level:
-                zero_crossings += 1
+        tries = 0
+        while tries < 2:
+            self.analog_input.readinto(self.input_buffer)
+            # Count zero crossings in our sample
+            zero_crossings = 0
+            above_avg = True
+            if self.input_buffer[0] < self.silence_level:
                 above_avg = False
-            elif i > self.silence_level and not above_avg:
-                zero_crossings += 1
-                above_avg = True
+            for i in self.input_buffer:
+                if above_avg and i < self.silence_level:
+                    zero_crossings += 1
+                    above_avg = False
+                elif i > self.silence_level and not above_avg:
+                    zero_crossings += 1
+                    above_avg = True
 
-        # Do the math for frequency, returning a bool 1 or 0
+            # Do the math for frequency, returning a bool 1 or 0
 
-        if zero_crossings > 32: # > 1600 hz
-            return False
-        else:
-            return True
-
+            if zero_crossings > 17: # > 1600 hz
+                return False
+            elif zero_crossings < 15:
+                return True
+            # If we're not returning an appropriate value, something got messed up, try reading again
+            tries += 1
+            if tries >= 4:
+                print("ERROR! BAD DATA INPUT!")
+                raise InterruptedError
+            
     def read_next_byte(self):
         self.byte_start_time = ticks_ms() # set when the last byte started
         byte_buff = 0
@@ -160,16 +173,20 @@ class TTYReader2:
             next_bit = ticks_ms() + self.bit_time
         return byte_buff
         
-    def check_for_input(self): # Returns false when input detected.
-        check = self.analog_input.value
-        if check > self.silence_level + self.noise_floor or check < self.silence_level - self.noise_floor:
-            return False 
-        return True 
-    
+    def decode_byte_array(self): # Decode the bytes in the byte array, returning their value. 
+	# Note: This will also empty the byte array, and reset the pointer!
+        output_str = ""
+        for i in self.read_byte_arr:
+            if i == 0b11111: # Hard-coded FIGS/LTRS switch because fuck itttttt
+                self.char_mode = self.FIGS
+            elif i == 0b10101:
+                self.char_mode = self.LTRS
+            else:
+                output_str += self.char_mode[i]
+        self.byte_pointer = 0		
+        return output_str
 
-
-
-
+  
     def monitor_loop(self):
         byte_timeout = 0
         continue_monitoring = self.calibrate() # If we fail to calibrate, it will not fire
@@ -191,21 +208,18 @@ class TTYReader2:
                 self.read_byte_arr[self.byte_pointer] = last_read_byte
                 self.byte_pointer += 1
 
-            while ticks_ms() < self.byte_start_time + self.bit_time:
-                pass # wait for the next byte to be possible.
+                while ticks_ms() < self.byte_start_time + self.bit_time:
+                    pass # wait for the next byte to be possible.
 
             if self.byte_pointer > 31: # we're at the end of the byte buffer, we gotta do somethin!!
-                self.decode_byte_array()
+                self.received_string += self.decode_byte_array()
 
-            if last_read_byte == 8 or byte_timeout > ticks_ms():
+            if last_read_byte == 8 or byte_timeout < ticks_ms():
                 # This is a carriage return character, for now we will let this be the end of the message
                 # TODO: handle this properly so a message can contain CRs.
                 continue_monitoring = False
             
-        self.decode_byte_array() # decode any text we've hit
+        self.received_string += self.decode_byte_array() # decode any text we've hit
         # we're free of the loop! 
-        return self.recieved_string 
+        return self.received_string 
             
-    def decode_byte_array(self): # NOT IMPLEMENTED YET! WILL DECODE BYTE ARRAY, RESET POINTER.
-        return "TODO"                
-
