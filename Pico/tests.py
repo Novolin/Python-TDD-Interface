cab81775d183862dfd632ac0b56b17de82b9ca6b
Novolin <captainnovolin@gmail.com>
2025-02-14 11:25:49 -0500

    testing change

--- Pico/tests.py
+++ Pico/tests.py
@@ -11,16 +11,31 @@ def read_mic():
     while time.ticks_diff(end_time, time.ticks_us()) > 0:
         nextsamp = time.ticks_add(time.ticks_us(), 50)
         samples.append(mic.read_u16())
-        while time.ticks_diff(nextsamp, time.ticks_us()) > 0:
-            pass # wait for the next sample time.
     took = time.ticks_diff(end_time, time.ticks_us())
     samp_num = len(samples)
     print(str(samp_num) + " samples collected, " + str(took) + " us over estimate")
     return samples
 
 
+def get_data_on_samples(sample_list:list):
+    zero_point = 32768
+    noise_floor = 100
+    zero_counts = 0
+    above_zero = True
+    for s in sample_list:
+        if s > zero_point + noise_floor and above_zero:
+            zero_counts += 1
+            above_zero = False
+        elif s < zero_point - noise_floor and not above_zero:
+            zero_counts += 1
+            above_zero = True
+    print("Counted " + str(zero_counts) + " crossings from " + str(len(sample_list)) + " samples.")
 
-
-
-def send_text(text:str):
-    pass
+    if zero_counts < 14: 
+        print("Less than 1400 Hz???")
+    elif zero_counts < 16:
+        print("Should be 1400Hz (Mark)")
+    elif zero_counts < 20:
+        print("Should be 1800Hz (Space)")
+    else:
+        print("More than 20 crossings, noise floor too low???")
